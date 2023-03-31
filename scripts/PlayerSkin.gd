f07443d3a827e6fe4061b5c18fa36294c93570e3
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-03-31 00:25:23 -0400

    Resource-defined player skins

---
+++ scripts/PlayerSkin.gd
@@ -0,0 +1,6 @@
+class_name PlayerSkin extends Resource
+
+@export var preview: Texture2D
+@export var spritesheet: Texture2D
+@export var frames := Vector2(6, 3)
+@export var reserved_for_id: String
